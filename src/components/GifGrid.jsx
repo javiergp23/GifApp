caab54f03cb3dae72253767c95e38f165110d19e
Javier Gonzalez <javgon2306@gmail.com>
2024-08-13 16:43:41 -0300

    add array gif

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,9 +1,15 @@
 
 export const GifGrid = ({ category }) => {
+    const gifs = [1,2,3,4,5,6,7,8,9,10];
+    
     return(
         <>
             <h3>{category}</h3>
-            <p>hola mundo</p>
+            {
+                gifs.map(gif => {
+                <p>{gif}</p>
+                })
+            }
         </>
     )
 }
