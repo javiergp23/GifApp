f5546a36e3ac3b7513fba0918c379d0f1bd7855c
Javier Gonzalez <javgon2306@gmail.com>
2024-08-15 17:57:43 -0300

    add url

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,14 +1,19 @@
 
 export const GifGrid = ({ category }) => {
-    const gifs = [1,2,3,4,5,6,7,8,9,10];
+    
+    const getGifs = async() => {
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=vVIHvNC7Yb5l94dtLK2fyblNzBRO33hH&q=${ category }`
+        const resp = await fetch(url);
+
+        console.log(resp);
+    }
+
+    getGifs();
+
     return(
         <>
             <h3>{category}</h3>
-            {
-                gifs.map(gif => {
-                <p>{gif}</p>
-                })
-            }
+           
         </>
     )
 }
