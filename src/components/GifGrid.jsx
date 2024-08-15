dd9518b31f4db2cb980a02c927fd18097349fa88
Javier Gonzalez <javgon2306@gmail.com>
2024-08-15 18:05:05 -0300

    changs url

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -2,7 +2,7 @@
 export const GifGrid = ({ category }) => {
     
     const getGifs = async() => {
-        const url = `https://api.giphy.com/v1/gifs/search?api_key=vVIHvNC7Yb5l94dtLK2fyblNzBRO33hH&q=${ category }`
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=  q=${ category }`
         const resp = await fetch(url);
 
         console.log(resp);
