61d14d8b1efd81b46125d39d1b067f54da1b352a
Javier Gonzalez <javgon2306@gmail.com>
2024-08-21 16:47:56 -0300

    changes_01

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -8,6 +8,7 @@ export const GifGrid = ({ category }) => {
         console.log(resp);
     }
 
+    
     getGifs();
 
     return(
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { GifExpertApp } from './GifExpertApp';
 import "../src/assets/style.css";
 
-
+// main
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
