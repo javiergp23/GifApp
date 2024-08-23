1bcdbdf02a6d8efd2553facc54810d5a57e3c710
Javier Gonzalez <javgon2306@gmail.com>
2024-08-23 15:59:24 -0300

    create getGifts

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,19 +1,13 @@
+import { getGifs } from "../helpers/getGifts"
 // agregand cada componente gifGrid
 export const GifGrid = ({ category }) => {
     
-    const getGifs = async() => {
-        const url = `https://api.giphy.com/v1/gifs/search?api_key=  q=${ category }`
-        const resp = await fetch(url);
-
-        console.log(resp);
-    }
-
-    getGifs();
+    getGifs(category);
 
     return(
         <>
             <h3>{category}</h3>
-            <p>Hola mundo</p>
+            
         </>
     )
 }
