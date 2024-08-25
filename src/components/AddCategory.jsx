36c3ebc6ec5f19e7bf4071f38c0e685aca30f387
Javier Gonzalez <javgon2306@gmail.com>
2024-08-25 11:09:47 -0300

    changes_06

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -18,14 +18,12 @@ export const AddCategory = ({ onNewCategory }) => {
     
   return (
     <form onSubmit={ (event) => onSubmit(event) }>
-
         <input 
             type="text" 
             placeholder="Buscar Gifs"
             value={inputValeu}    
             onChange={ onInputChange}
         />
-        
     </form>
   )
 }
