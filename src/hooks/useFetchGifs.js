e07240606212b837b1b7a21df7f0fb2cf5c904a5
Javier Gonzalez <javgon2306@gmail.com>
2024-08-31 13:04:41 -0300

    change useFetchGifs

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -4,7 +4,6 @@ import { getGifs } from "../helpers/getGifts";
 export const useFetchGifs = ( category) => {
         const [images, setImages] = useState([]);  
         const [isLoading, setIsLoading] = useState(true);  
-        
         const getImages = async () => {
             const newImages = await getGifs(category);
             setImages(newImages);
